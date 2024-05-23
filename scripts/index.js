084e7429b7d266a69482e7ec007d84a349cb1f65
mohamedo22 <oooomohamed421@gmail.com>
2024-05-23 11:39:14 +0300

    First adding

---
+++ scripts/index.js
@@ -0,0 +1,121 @@
+let header = document.querySelector("header");
+header.addEventListener('mouseenter' , function(){
+    header.style.backgroundColor = 'white';
+    let PinHeader = document.querySelectorAll("header p").forEach(function(el)
+    {
+        el.style.color = 'black';
+    });
+    document.querySelectorAll(".icons path").forEach((el)=>{
+        el.style.fill = 'black';
+    });
+    document.querySelector('.bag path').style.stroke = 'black';
+    document.querySelector('.bag path').style.fill = 'black';
+});
+header.addEventListener('mouseleave', function(){
+    let header = document.querySelector("header");
+    header.style.backgroundColor = '';
+    let PinHeader = document.querySelectorAll("header p").forEach(function(el)
+    {
+        el.style.color = '';
+    });
+    document.querySelectorAll(".icons path").forEach((el)=>{
+        el.style.fill = '';
+    });
+    document.querySelector('.bag path').style.stroke = '';
+    document.querySelector('.bag path').style.fill = '';
+   
+});
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 5) {
+        header.style.backgroundColor = 'white';
+        header.style.color = 'black';
+        let PinHeader = document.querySelectorAll("header p").forEach(function(el)
+        {
+            el.style.color = 'black';
+        });
+        document.querySelectorAll(".icons path").forEach((el)=>{
+            el.style.fill = 'black';
+        });
+        document.querySelector('.bag path').style.stroke = 'black';
+        document.querySelector('.bag path').style.fill = 'black';
+    } else {
+        let header = document.querySelector("header");
+        header.style.backgroundColor = '';
+        header.style.color = '';
+        let PinHeader = document.querySelectorAll("header p").forEach(function(el)
+        {
+            el.style.color = '';
+        });
+        document.querySelectorAll(".icons path").forEach((el)=>{
+            el.style.fill = '';
+        });
+        document.querySelector('.bag path').style.stroke = '';
+        document.querySelector('.bag path').style.fill = '';
+       
+    }
+});
+let el = 0;
+function NextElement(elementNumber)
+{
+    el++;
+    console.log(el);
+    if(el > 5)
+    {
+            el=0;
+    }
+    if(window.innerWidth >= 1000)
+    {
+            document.querySelector(`.wrapper${elementNumber}`).style.marginLeft = `${-el*25}rem`;
+    }
+    else
+    {
+        document.querySelector(`.wrapper${elementNumber}`).style.marginLeft = `${-el*26}rem`;
+    }
+    document.querySelectorAll(`.thempn${elementNumber}`).forEach((el)=>
+    {
+        el.style.backgroundColor = 'silver';
+    });
+    document.querySelector(`.ThembNav${el}${elementNumber}`).style.backgroundColor = 'black';
+}
+function LastElement(elementNumber)
+{
+    el--;
+    console.log(el);
+    if(el < 0)
+    {
+            el=5;
+    }
+    if(window.innerWidth >= 1000)
+        {
+                document.querySelector(`.wrapper${elementNumber}`).style.marginLeft = `${-el*25}rem`;
+        }
+        else
+        {
+            document.querySelector(`.wrapper${elementNumber}`).style.marginLeft = `${-el*26}rem`;
+        }
+    document.querySelectorAll(`.thempn${elementNumber}`).forEach((el)=>
+        {
+            el.style.backgroundColor = 'silver';
+        });
+        document.querySelector(`.ThembNav${el}${elementNumber}`).style.backgroundColor = 'black';
+}
+document.querySelectorAll(".ContainerOFinksFooter:not(.formContainer)").forEach((el) => {
+    el.addEventListener("click", function() {
+        let height = el.style.height;
+        if (height === '1rem') {
+            el.style.height = '10rem';
+        } else {
+            el.style.height = '1rem';
+        }
+    });
+});
+function openSearchWindow()
+{
+    let searchWindow = document.querySelector('.search');
+    searchWindow.style.visibility = 'visible';
+}
+function closeSearchWindow()
+{
+    let searchWindow = document.querySelector('.search');
+    searchWindow.style.visibility = '';
+}
